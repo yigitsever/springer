72f321de98a8af11fabab075a3732839e1918273
Yigit Sever <yigit.sever@tedu.edu.tr>
2020-05-10 16:11:26 +0300

    Cleanup

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # springer
-Python script to donwload you want Springer Books released for free during the 2020 COVID-19 quarantine
+Python script to download Springer Books released for free during the 2020 COVID-19 quarantine
 
 # Download excel file from:
 https://www.springernature.com/gp/librarians/news-events/all-news-articles/industry-news-initiatives/free-access-to-textbooks-for-institutions-affected-by-coronaviru/17855960
 
-You can filter and copy donwload links saved as links.txt
-If you saved another name file you should change links.txt file name in line 13
-The python script and links' files must be same directory
+or
 
-Not: the script working on python3 becaus of urllib.request module
+```bash
+curl https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v5 -o books.xlsx
+```
+
+You can filter and copy download links saved as `links.txt`
+If you saved another name file you should change `links.txt` file name in line 13
+The python script and `links.txt` files must be under the same directory
+
+Not: the script requires python3 because of `urllib.request` module
--- main.py
+++ main.py
@@ -4,36 +4,56 @@
 # the py and links' files must be same directory
 # I do not check working another lang books, only eng books
 ############################################################
-import urllib.request
-import urllib.parse
+import os
 import subprocess
+import urllib.parse
+import urllib.request
 
-path = 'pwd'
-alink='https://link.springer.com/'
-links = 'links.txt'
+path = "pwd"
+alink = "https://link.springer.com/"
+links = "links.txt"
+
+
 def direc():
     global path
     out = subprocess.run([path], stdout=subprocess.PIPE)
-    path = (out.stdout.decode('utf-8'))[:-1]+'/'
+    path = (out.stdout.decode("utf-8"))[:-1] + "/"
     print(path)
 
+
 def website():
-    direc() # current directory
-    files = open(path+links,'r') # open file contains download links
-    count = 0
-    for link in files.readlines():
-        site = urllib.request.urlopen(link)
-        web=site.read().decode('utf-8')
-        start=web.find('<h1>')
-        end=web.find('</h1>')
-        title = web[start+4:end]
-        while title.find('/')>0: # some title contains '/', so this is a problem such as 'does not exist directory'
-            title = title.replace('/','-')
-        title=title.replace(' ','_')+'.PDF'
-        start = web.find('content/pdf')
-        end = web.find('\"',start)
-        linkd = alink+web[start:end]
-        urllib.request.urlretrieve(linkd, path+title) # download and create file on local
-        print(str(count) + ':' +title)
-        count+=1
-    files.close()
+    direc()  # current directory
+    try:
+        # open file that contains download links
+        with open(os.path.join(path, links), "r") as files:
+            count = 0
+            for line in files.readlines():
+                if not line.strip():
+                    # file ends with a newline
+                    exit(0)
+                site = urllib.request.urlopen(line)
+                web = site.read().decode("utf-8")
+                start = web.find("<h1>")
+                end = web.find("</h1>")
+                title = web[start + 4 : end]
+
+                # some title contains '/', so this is a problem such as 'does not exist
+                # directory'
+                while title.find("/") > 0:
+                    title = title.replace("/", "-")
+                title = title.replace(" ", "_") + ".PDF"
+                start = web.find("content/pdf")
+                end = web.find('"', start)
+                linkd = alink + web[start:end]
+
+                # download and create file on local
+                urllib.request.urlretrieve(linkd, path + title)
+
+                print(str(count) + ":" + title)
+                count += 1
+    except IOError:
+        raise SystemExit("cannot find links.txt")
+
+
+if __name__ == "__main__":
+    website()
